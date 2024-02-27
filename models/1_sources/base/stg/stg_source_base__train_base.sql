3f06a1ffa438196e741ed637bd0c38255700e61a
nathandxn <nathandxn@gmail.com>
2024-02-26 22:57:03 -0500

    add stg model

---
+++ models/1_sources/base/stg/stg_source_base__train_base.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('base', 'train_base') }}
+
+)
+
+, stg as (
+
+    select
+          s.case_id
+        , s.date_decision as decision_date
+        , s.month as decision_month
+        , s.week_num as week_number
+        , s.target as is_default
+
+    from source as s
+
+)
+
+select * from stg
