9cba96210a1adb76a91f7a45b889f734f387692a
nathandxn <nathandxn@gmail.com>
2024-02-29 21:44:29 -0500

    update columns

--- models/1_sources/apply_prev/stg/stg_source_apply_prev__1_0.sql
+++ models/1_sources/apply_prev/stg/stg_source_apply_prev__1_0.sql
@@ -11,17 +11,22 @@ with source as (
         , s.case_id
         , s.num_group1 as num_group_1
         , s.approvaldate_319D as approval_date
+        , s.creationdate_885D as created_date
+        , s.dateactivated_425D as contract_activation_date
+        , s.dtlastpmt_581D as last_payment_date
+        , s.dtlastpmtallstes_3545839D as last_payment_date_all
         , s.status_219L as prev_apply_status
         , s.actualdpd_943P as actual_days_past_due_of_prev_contract
         , s.annuity_853A as monthly_annuity
 
         , nvl2(s.approvaldate_319D, 1, 0)::boolean as was_approved
+        , nvl2(s.dateactivated_425D, 1, 0)::boolean as was_contract_activated
 
 /*
     , byoccupationinc_3656910L NUMBER(38, 2)
     , cancelreason_3545846M VARCHAR(36)
     , childnum_21L NUMBER(38, 2)
-    , creationdate_885D DATE
+
     , credacc_actualbalance_314A NUMBER(38, 2)
     , credacc_credlmt_575A NUMBER(38, 2)
     , credacc_maxhisbal_375A NUMBER(38, 2)
@@ -31,11 +36,10 @@ with source as (
     , credamount_590A NUMBER(38, 2)
     , credtype_587L VARCHAR(36)
     , currdebt_94A NUMBER(38, 2)
-    , dateactivated_425D DATE
+
     , district_544M VARCHAR(36)
     , downpmt_134A NUMBER(38, 2)
-    , dtlastpmt_581D DATE
-    , dtlastpmtallstes_3545839D	DATE
+
     , education_1138M VARCHAR(36)
     , employedfrom_700D DATE
     , familystate_726L VARCHAR(36)
