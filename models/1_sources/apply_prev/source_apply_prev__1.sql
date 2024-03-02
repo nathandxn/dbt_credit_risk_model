42565bb5e27f062ff4d1c48e566a94421647902a
nathandxn <nathandxn@gmail.com>
2024-03-02 14:00:24 -0500

    union train models

--- models/1_sources/apply_prev/source_apply_prev__1.sql
+++ models/1_sources/apply_prev/source_apply_prev__1.sql
@@ -4,10 +4,18 @@
         ) 
     }}
 
-with stg as (
+with stg_0 as (
 
     select * from {{ ref('stg_source_apply_prev__1_0') }}
 
 )
 
-select * from stg
+, stg_1 as (
+
+    select * from {{ ref('stg_source_apply_prev__1_1') }}
+
+)
+
+select * from stg_0
+union all
+select * from stg_1
