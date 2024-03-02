12c3a4404270d6dbcf19d39c2c418fb7b8352244
nathandxn <nathandxn@gmail.com>
2024-03-02 13:56:44 -0500

    add rest of fields

--- models/1_sources/apply_prev/stg/stg_source_apply_prev__1_0.sql
+++ models/1_sources/apply_prev/stg/stg_source_apply_prev__1_0.sql
@@ -20,6 +20,8 @@ with source as (
         , s.status_219L as prev_apply_status
         , s.credtype_587L as credit_type
         , s.credacc_status_367L as prev_credit_account_status
+        , s.rejectreason_755M as reject_reason
+        , s.rejectreasonclient_4145042M as reject_reason_client
         , s.cancelreason_3545846M as applicant_cancel_reason
         , s.inittransactioncode_279L as initial_transaction_code
         , s.district_544M as district_of_applicant_address
@@ -29,6 +31,7 @@ with source as (
         , s.postype_4733339M as type_of_point_of_sale
 
         , s.pmtnum_8L as count_payments_made
+        , s.tenor_203L as count_installments_in_prev_application
         , s.maxdpdtolerance_577P as max_days_past_due
         , s.outstandingdebt_522A as amount_outstanding_debt
         , s.credamount_590A as loan_amount_or_card_limit
@@ -37,6 +40,7 @@ with source as (
         , s.credacc_credlmt_575A as credit_card_limit_prev_application
         , s.credacc_maxhisbal_375A as max_historical_balance
         , s.credacc_minhisbal_90A as min_historical_balance
+        , s.revolvingaccount_394A as applicant_revolving_account
 
         , s.mainoccupationinc_437A as client_main_income_amount
         , s.byoccupationinc_3656910L as applicant_occupation_income
@@ -52,18 +56,6 @@ with source as (
 
         , nvl2(s.approvaldate_319D, 1, 0)::boolean as was_approved
         , nvl2(s.dateactivated_425D, 1, 0)::boolean as was_contract_activated
-
-/*
-
-
-    , rejectreason_755M	VARCHAR(36)
-    , rejectreasonclient_4145042M VARCHAR(36)
-
-    , revolvingaccount_394A	NUMBER(38, 2)
-
-    , tenor_203L NUMBER(38, 2)
-
-*/
 
     from source as s
 
