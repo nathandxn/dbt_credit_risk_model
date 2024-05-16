838d343291b8db0449a123a627379b3f09b16537
nathandxn <nathandxn@gmail.com>
2024-05-15 22:59:54 -0400

    feat: add additional static fields

--- models/2_modeling/modeling_case_default_prediction.sql
+++ models/2_modeling/modeling_case_default_prediction.sql
@@ -33,9 +33,18 @@ with base as (
 
         -- features
         , fs.actual_days_past_due_with_tolerance as static_actual_days_past_due_with_tolerance
-
-
-
+        , fs.monthly_annuity_amount as static_monthly_annuity_amount
+        , fs.next_month_annuity_amount as static_next_month_annuity_amount
+        , fs.count_apply_same_email as static_count_apply_same_email
+        , fs.count_apply_last_30_days as static_count_apply_last_30_days
+        , fs.count_apply_same_phone_number as static_count_apply_same_phone_number
+        , fs.count_apply_same_employer as static_count_apply_same_employer
+        , fs.count_apply_same_employer_last_7_days as static_count_apply_same_employer_last_7_days
+        , fs.count_apply_same_mobile_phone_number as static_count_apply_same_mobile_phone_number
+        , fs.avg_days_past_or_before_due_of_paymt_last_24_months as static_avg_days_past_or_before_due_of_paymt_last_24_months
+        , fs.avg_days_past_or_before_due_of_paymt_last_3_months as static_avg_days_past_or_before_due_of_paymt_last_3_months
+        , fs.avg_days_past_or_before_due_of_paymt_last_24_months_with_tolerance as static_avg_days_past_or_before_due_of_paymt_last_24_months_with_tolerance
+        , fs.avg_days_past_due_of_paymt_last_24_months_with_tolerance_from_max_close_date as static_avg_days_past_due_of_paymt_last_24_months_with_tolerance_from_max_close_date
 
     from base as b
     left join feat_static as fs
