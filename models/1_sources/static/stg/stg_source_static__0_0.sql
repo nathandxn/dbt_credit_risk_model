bc5e1104fa567a1ada68f4f635db012ffa08c5a2
nathandxn <nathandxn@gmail.com>
2024-05-15 22:22:43 -0400

    feat: add additional static fields

--- models/1_sources/static/stg/stg_source_static__0_0.sql
+++ models/1_sources/static/stg/stg_source_static__0_0.sql
@@ -21,6 +21,15 @@ with source as (
         , s.applicationscnt_867L as count_apply_same_mobile_phone_number
         , s.avgdbddpdlast24m_3658932P as avg_days_past_or_before_due_of_paymt_last_24_months
         , s.avgdbddpdlast3m_4187120P as avg_days_past_or_before_due_of_paymt_last_3_months
+        , s.avgdbdtollast24m_4525197P as avg_days_past_or_before_due_of_paymt_last_24_months_with_tolerance
+        , s.avgdpdtolclosure24_3658938P as avg_days_past_due_of_paymt_last_24_months_with_tolerance_from_max_close_date
+        , s.avginstallast24m_3658937A as avg_instals_paid_last_24_months
+        , s.avglnamtstart24m_4525187A as avg_loan_amt_last_24_months
+        , s.avgmaxdpdlast9m_3716943P as avg_days_past_due_last_9_months
+        , s.avgoutstandbalancel6m_4187114A as avg_outstanding_bal_last_6_months
+        , s.avgpmtlast12m_4525200A as avg_of_pays_made_last_12_months
+        , s.bankacctype_710L as type_of_bank_account
+        , s.cardtype_51L as type_of_credit_card
 
     from source as s
 
